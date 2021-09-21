becd7f3fa45f5e6c0f2bd2fab929f81f360ce2a6
yeehui.poh <yhpoh91@gmail.com>
2021-09-22 01:52:56 +0800

    remove auth for verifying token

--- src/services/authenticator.js
+++ src/services/authenticator.js
@@ -1,65 +1,17 @@
 const axios = require('axios');
-const jsonwebtoken = require('jsonwebtoken');
 
 const { L } = require('./logger')('Authenticator');
 
 
 const authenticationEnabled = (process.env.AUTHENTICATION_ENABLED || 'true').toLowerCase() === 'true';
-const earlyTokenRefreshSeconds = parseInt(process.env.SELF_TOKEN_EARLY_REFRESH_SECONDS || '30', 10);
-const selfClientId = process.env.CLIENT_ID;
-const selfClientSecret = process.env.CLIENT_SECRET;
-const authenticatorLoginServiceUrl = process.env.AUTHENTICATOR_SERVICE_LOGIN_SERVICE_URL;
 const authenticatorVerifyUrl = process.env.AUTHENTICATOR_SERVICE_VERIFY_URL;
-
-let currentSelfToken;
-let currentSelfTokenExp = 0;
-
-const getServiceToken = async () => {
-  try {
-    const now = Math.floor(new Date().getTime() / 1000);
-    if (currentSelfToken == null || currentSelfTokenExp < now) {
-      const body = {
-        clientId: selfClientId,
-        clientSecret: selfClientSecret,
-      };
-      const response = await axios.post(authenticatorLoginServiceUrl, body);
-      const { data } = response;
-      const token = data.access_token;
-
-      const payload = jsonwebtoken.decode(token);
-
-      // Save Current Token
-      currentSelfToken = token;
-      currentSelfTokenExp = payload.exp - earlyTokenRefreshSeconds;
-    }
-
-    return Promise.resolve(currentSelfToken);
-  } catch (error) {
-    return Promise.reject(error);
-  }
-}
-
-const getConfig = async () => {
-  try {
-    const token = await getServiceToken();
-    const config = {
-      headers: {
-        "Authorization": `Bearer ${token}`,
-      },
-    };
-    return Promise.resolve(config);
-  } catch (error) {
-    return Promise.reject(error);
-  }
-}
 
 const validateToken = async (accessToken) => {
   try {
     const url = authenticatorVerifyUrl;
     const body = { accessToken };
-    const config = await getConfig();
 
-    const response = await axios.post(url, body, config);
+    const response = await axios.post(url, body);
     const { data } = response;
     return Promise.resolve(data);
   } catch (error) {
@@ -96,7 +48,6 @@ const authenticate = async (req, res, next) => {
       L.debug('Validating Token');
       const { ok: isValidated, payload } = await validateToken(token);
       if (isValidated && payload) {
-        L.debug('Token is Valid');
         const { sub, typ } = payload;
 
         hasAccess = true;
@@ -111,6 +62,7 @@ const authenticate = async (req, res, next) => {
       return;
     }
 
+    L.debug('Token is Valid');
     next();
   } catch (error) {
     L.error(error.message); 
